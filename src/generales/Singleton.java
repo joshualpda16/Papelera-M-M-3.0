a76f3e9239d066f8efa9c289ded056ba4433171a
joshua.d.juanchini <joshua.d.juanchini@MW7KGBYEEKZRMK.dir.svc.accenture.com>
2014-10-30 12:39:02 -0300

    Se agregó el formulario de configuración con su método de show/hide

--- src/generales/Singleton.java
+++ src/generales/Singleton.java
@@ -5,24 +5,55 @@
  */
 package generales;
 
+import forms.frmConfiguracion;
+
 /**
  *
  * @author joshua.d.juanchini
  */
 public class Singleton {
-    
+
+    //Formularios
+
+    private JfrPrincipal framePrincipal = null;
+    private frmConfiguracion frameConfiguracion = null;
+
+    //Parámetros generales
+    //<editor-fold defaultstate="collapsed" desc="Singleton Things">
     private Singleton() {
-        
+
     }
-    
+
     public static Singleton getInstance() {
         return singletonHolder.INSTANCE;
     }
-    
+
     private static class singletonHolder {
+
         private static final Singleton INSTANCE = new Singleton();
     }
-    
-    
-    
+
+//</editor-fold>
+    public void showPrincipal() {
+        framePrincipal = new JfrPrincipal();
+        framePrincipal.run();
+    }
+
+    public void changeLookAndFeel(String lf) {
+        framePrincipal.lookandfeel(lf);
+    }
+
+    public void frConfiguracion() {
+        if (frameConfiguracion == null) {
+            frameConfiguracion = new frmConfiguracion();
+            framePrincipal.jDesktopPane1.add(frameConfiguracion);
+        }
+        if(frameConfiguracion.estado){
+            frameConfiguracion.show();
+            frameConfiguracion.estado=false;
+        } else{
+            frameConfiguracion.hide();
+            frameConfiguracion.estado=true;
+        }
+    }
 }
