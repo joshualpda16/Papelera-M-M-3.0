361683684024cbfded6ef1c5c5d27c17604e6bdb
joshua.d.juanchini <joshua.d.juanchini@MW7KGBYEEKZRMK.dir.svc.accenture.com>
2014-10-30 12:11:00 -0300

    Creado el método público "lookandfeel" Se puede llamar desde otros formularios para modificar el look and feel del principal.

--- src/generales/JfrPrincipal.java
+++ src/generales/JfrPrincipal.java
@@ -6,6 +6,11 @@
 package generales;
 
 import heredClasses.PintarJDP;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -19,6 +24,23 @@ public class JfrPrincipal extends javax.swing.JFrame {
     public JfrPrincipal() {
         initComponents();
         jDesktopPane1.setBorder(new PintarJDP("/images/fondo3.jpg"));
+        lookandfeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+    }
+    
+    public void lookandfeel(String lf){
+        try {
+            UIManager.setLookAndFeel(lf);
+            SwingUtilities.updateComponentTreeUI(this);
+            this.pack();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(JfrPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(JfrPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(JfrPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(JfrPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public void run() {
@@ -131,4 +153,8 @@ public class JfrPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenu mnuAyuda;
     private javax.swing.JMenuItem mnuSalir;
     // End of variables declaration//GEN-END:variables
+
+    private void lookandfeel() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
